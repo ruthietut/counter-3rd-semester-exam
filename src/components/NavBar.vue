a476d40280892aa2440f6b93395bf391297a1159
Ruth Obasi <88226737+ruthietut@users.noreply.github.com>
2023-03-09 16:13:12 +0100

    update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,9 @@
 <template>
   <nav>
-    <router-link active-class="active" to="/"
-      ><img class="img" src="../assets/altschool-logo.png" alt="logo"
-    /></router-link>
-
     <ul>
+      <router-link active-class="active" to="/"
+        ><img class="img" src="../assets/altschool-logo.png" alt="logo"
+      /></router-link>
       <li><router-link active-class="active" to="/">Home</router-link></li>
       <li>
         <router-link active-class="active" to="/counter">Counter</router-link>
